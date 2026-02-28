eb3f1ff3b02ab2bd4ed8fa73c515c51a2265c168
Anton Michna <antonmichna@gmail.com>
2026-02-28 16:40:45 -0600

    adfs

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   const port = process.env.PORT || 8080;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
