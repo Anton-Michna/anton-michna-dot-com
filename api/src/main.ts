017fa80dbf989972c6438d9c1bb3588eaf54b0db
Anton Michna <antonmichna@gmail.com>
2026-04-03 17:58:06 -0500

    Fix: Bind to 0.0.0.0 for Docker deployment

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,7 +11,8 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   const port = process.env.PORT || 8080;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: http://0.0.0.0:${port}`);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
